1b3405e4135ffa84fcdeff7dd2793213781f0a5a
Abraham <abraham.armando.dominguez.rodriguez@uabc.edu.mx>
2026-04-16 16:14:03 -0700

    avatares

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3989441477" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4275433735" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
